79f037e4b43a088067cfe1ed431dad2c9c7c406d
Hasanka Piyumal <hasankapiyumal7@gmail.com>
2023-11-20 23:25:38 +0530

    This is first commit creating admin add product

---
+++ app/src/main/java/com/zaviron/burgershotadminapp/AddProductActivity.java
@@ -0,0 +1,14 @@
+package com.zaviron.burgershotadminapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+    }
+}
